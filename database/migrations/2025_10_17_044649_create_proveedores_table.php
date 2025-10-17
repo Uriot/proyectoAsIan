5d68cd8dc86d4b8e5e8a14c7f1f2bcb919255a2b
uriot <elliotur@gmail.com>
2025-10-16 23:22:01 -0600

    migrations for Er

---
+++ database/migrations/2025_10_17_044649_create_proveedores_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('proveedores', function (Blueprint $table) {
+            $table->id();
+            $table->string('nombre')->unique();
+            $table->string('contacto')->nullable();
+            $table->string('direccion')->nullable();
+            $table->string('telefono')->nullable();
+            $table->string('email')->nullable();
+            $table->string('nit')->nullable();
+            $table->string('creado_por')->nullable();
+            $table->string('actualizado_por')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('proveedores');
+    }
+};
