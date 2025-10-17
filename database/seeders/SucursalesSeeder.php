42fc8ec76d99ec78e8f3f476aee83921dc5d3a06
Ian <elliotur@gmail.com>
2025-10-17 04:15:29 -0600

    Seeders actualizados

--- database/seeders/SucursalesSeeder.php
+++ database/seeders/SucursalesSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Sucursal;
 
 class SucursalesSeeder extends Seeder
 {
@@ -13,7 +14,14 @@ class SucursalesSeeder extends Seeder
     public function run(): void
     {
         $data = [
-            ['nombre' => 'Plaza Americas', 'direccion' => 'carretera interamericana, km 12, Mazatenango, Suchitepéquez', 'telefono' => '1234-5678', 'email' => 'plazaamericas@tecnoflow.com', 'ciudad' => 'Mazatenango', 'creado_por' => 'ian@mail.com', 'actualizado_por' => null],
+            ['nombre' => 'Plaza Americas', 'direccion' => 'Carretera Interamericana, km 12, Mazatenango, Suchitepéquez', 'telefono' => '1234-5678', 'email' => 'plazaamericas@tecnoflow.com', 'ciudad' => 'Mazatenango', 'creado_por' => 'urizarian@gmail.com', 'actualizado_por' => null],
+            ['nombre' => 'TecnoFlow Central', 'direccion' => '6a Avenida 4-55, Zona 1, Ciudad de Guatemala', 'telefono' => '2222-3344', 'email' => 'central@tecnoflow.com', 'ciudad' => 'Ciudad de Guatemala', 'creado_por' => 'urizarian@gmail.com', 'actualizado_por' => null],
+            ['nombre' => 'Sucursal Quetzaltenango', 'direccion' => '4a Calle 8-22, Zona 3, Quetzaltenango', 'telefono' => '7766-5544', 'email' => 'quetzaltenango@tecnoflow.com', 'ciudad' => 'Quetzaltenango', 'creado_por' => 'urizarian@gmail.com', 'actualizado_por' => null],
+            ['nombre' => 'Sucursal Escuintla', 'direccion' => 'Avenida Centroamérica 3-10, Zona 2, Escuintla', 'telefono' => '7888-9911', 'email' => 'escuintla@tecnoflow.com', 'ciudad' => 'Escuintla', 'creado_por' => 'urizarian@gmail.com', 'actualizado_por' => null],
         ];
+
+        foreach ($data as $item) {
+            Sucursal::create($item);
+        }
     }
 }
