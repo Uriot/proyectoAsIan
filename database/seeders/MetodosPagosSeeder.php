387ddea9e83bb5526a2fd954dba5cccc889cb70f
uriot <elliotur@gmail.com>
2025-10-17 00:32:54 -0600

    archives seeders

---
+++ database/seeders/MetodosPagosSeeder.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class MetodosPagosSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        //
+    }
+}
